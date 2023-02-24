a5aca06e013853346d697f31be97e91dba6ebc12
Grace Joh <graceehjoh@gmail.com>
2023-02-24 10:16:49 -0600

    feat: Add Book #initialize

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,9 @@
 class Book
-  
+  attr_reader :title, :author, :publication_year
+
+  def initialize(book_hash)
+    @title = book_hash.fetch(:title, nil)
+    @author = "#{book_hash.fetch(:author_first_name, nil)} #{book_hash.fetch(:author_last_name, nil)}"
+    @publication_year = book_hash.fetch(:publication_date).split(' ').last || nil
+  end
 end
