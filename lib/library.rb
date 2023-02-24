46ed0b908c8d0c7b38fb20a3d5c758f8abc8e919
Grace Joh <graceehjoh@gmail.com>
2023-02-24 11:38:38 -0600

    refactor: Library #add_author

--- lib/library.rb
+++ lib/library.rb
@@ -6,4 +6,25 @@ class Library
     @books = []
     @authors = []
   end
+
+  def add_author(author)
+    @authors << author
+  end
+
+  # books
+
+  def publication_time_frame_for(name)
+    search_author = nil
+    authors.each do |author|
+      break search_author = author if author.name == name
+    end
+
+    return 'Our library has no author by that name' if search_author.nil?
+    return "#{search_author.name} has no books in our library" if search_author.books.empty?
+    return "#{search_author.name} has one book in our library published on #{search_author.books.first.publication_year}" if search_author.books.count == 1
+
+    search_author.books.sort_by!(&:publication_year)
+    publication_hash = { start: search_author.books.first.publication_date,
+                         end: search_author.books.last.publication_date }
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -21,4 +21,23 @@ RSpec.describe Library do
       expect(@dallas_public.authors).to eq([])
     end
   end
+
+  describe '#add_author' do
+    it 'creates an author object and adds it to the authors array' do
+      charlotte_bronte = Author.new({ first_name: 'Charlotte', last_name: 'Bronte' })
+
+      expect(@denver_public.add_author(charlotte_bronte)).to be_an Array
+      expect(@denver_public.authors).to eq([charlotte_bronte])
+    end
+  end
+
+  describe '#publication_time_frame_for' do
+    before(:each) do
+      @denver_public.add_author('Charlotte Bronte')
+    end
+  
+    it 'returns a hash of start and end publication dates' do
+
+    end
+  end
 end
